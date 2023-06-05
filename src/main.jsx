5b573712ada16944f29da7a90b394b29fe908463
davidfer <davidfernando1112@gmail.com>
2023-06-05 14:47:32 -0500

    prueb despli

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <h1>hola como te encuntras</h1>
+    <h2>no entiendo nada</h2>
   </React.StrictMode>,
 )
